c3bf9c3073520c887c9f3313beef2d97fee1c6f1
ByteWiizard <Bytewiizard@gmail.com>
2024-05-23 08:44:54 +0000

    Changes

--- server/routes/safetyComplaint.js
+++ server/routes/safetyComplaint.js
@@ -41,8 +41,8 @@ router.post("/", async (req, res) => {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: "yaman1337.goyal@gmail.com",
+        pass: "vpzn lsdk ipjt",
       },
     });
 
