b077780cebf86c6c011611d38042daf58bbcb8dd
bytewiizard <bytewiizard@gmail.com>
2024-12-21 10:55:21 +0530

    daily work record template correction and pdf drive sharing update

--- client/src/Components/Pdf/ExposurePlan.jsx
+++ client/src/Components/Pdf/ExposurePlan.jsx
@@ -5,7 +5,7 @@ const ExposurePlan = () => {
         <div className='w-full h-[600px] md:h-screen flex justify-center items-center mt-32 mb-8'>
             <iframe
                 className='w-full md:w-3/4 lg:w-1/2 h-full'
-                src="https://drive.google.com/file/d/1vW1QJ6bAqDSWgv0ZV1Q_bNGIoyXDjlHP/preview"
+                src="https://drive.google.com/file/d/1j0EolVBUO0kngleoNLugctttlVzt37_p/preview"
                 frameBorder="0"
             ></iframe>
         </div>
--- client/src/Components/Pdf/TermsAndConditions.jsx
+++ client/src/Components/Pdf/TermsAndConditions.jsx
@@ -5,7 +5,7 @@ const TermsAndConditions = () => {
     <div className='w-full h-96 md:h-screen flex justify-center items-center mt-32 mb-8'>
       <iframe
         className='w-full md:w-3/4 lg:w-1/2 h-full'
-        src="https://drive.google.com/file/d/1cjdWUpgE4dTd8_7gTqindeCBHRR03hNR/preview"
+        src="https://drive.google.com/file/d/1k7Lz1d2wFtbMLDglCLvzFI8XoxPJnM4D/preview"
         frameBorder="0"
       ></iframe>
     </div>
